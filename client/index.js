59a56d12857babaf788ff786fcf3341094315f5e
alibhatti99 <alibhatti99@gmail.com>
2016-09-21 18:07:14 -0700

    added-client-files

---
+++ client/index.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { browserHistory } from 'react-router'
+// import routes from './routes'
+// import ReactStormpath, { Router } from 'react-stormpath';
+
+// ReactStormpath.init();
+
+// ReactDOM.render(
+//   <Router history={browserHistory} routes={routes}>
+//   </Router>
+//   ,document.getElementById('app'));
---
+++ client/routes.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import App from './components/App';
+// import CreateEvent from './components/CreateEvent';
+// import EventPromoter from './components/EventPromoter';
+// import EventDetails from './components/EventDetails';
+// import UserDetails from './components/UserDetails';
+// import Index from './components/Index';
+// import { Route, IndexRoute } from 'react-router';
+// import { LogoutRoute, AuthenticatedRoute } from 'react-stormpath';
+
+// export default (
+//   <Route path="/" component={App}>
+//     <IndexRoute component={Index} />
+//     <AuthenticatedRoute>
+//       <LogoutRoute path="/logout" />
+//       <Route path ="/create" component={CreateEvent} />
+//       <Route path ="/eventDetails" component={EventDetails} />
+//       <Route path ="/profile" component={UserDetails} />
+//     </AuthenticatedRoute>
+//   </Route>
+// );
