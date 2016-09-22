6e74e41e8f7d15ae3d40dbdfe8084520ebdf8787
Wes Young <wes.young14@gmail.com>
2016-09-22 13:12:52 -0600

    modified user and adventure schemas

--- server/models/Users.js
+++ server/models/Users.js
@@ -3,10 +3,13 @@ var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({
   first: String,
   last: String,
+  username: String,
   email: String,
   points: Number,
   level: Number,
-  password: String
+  password: String,
+  myAdventures: [{ type: ObjectId, ref: 'Adventure' }],
+  currentAdventures: [{ type: ObjectId, ref: 'Adventure' }]
 });
 
 var User = mongoose.model('User', UserSchema);
