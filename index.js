18b145c833f95bae2c91ca0bd8c060ae0e665efe
Kyle Van Vleck <kylevanvleck@gmail.com>
2016-09-21 21:21:53 -0500

    index server file set up

---
+++ index.js
@@ -0,0 +1,16 @@
+var app = require('./server/server.js');
+var mongoose = require('mongoose');
+
+// set mongoURI (env variable name may be different for AWS)
+var mongoURI = process.evn.MONGODB_URI || 'mongodb://localhost/treasuretrek';
+
+// connect to the db
+mongoose.connect(mongoURI);
+
+// set port
+var port = process.env.PORT || 1337;
+
+// listen on port
+app.list(port);
+
+console.log("Server is listening on port " + port);
