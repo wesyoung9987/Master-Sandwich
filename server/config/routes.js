8b359f2b0911756a66168ce71497e18ea82cb5d4
Kyle Van Vleck <kylevanvleck@gmail.com>
2016-09-21 21:35:47 -0500

    set up stub for server routes

---
+++ server/config/routes.js
@@ -0,0 +1,11 @@
+var userHandler = require('../users/userHandler.js');
+var riddleHandler = require('../lists/riddleHandler.js');
+
+// export entire function to server.js
+module.exports = function(app, express) {
+
+  // TODO:
+  // fill out api requests
+  // direct to correct server method
+
+}
