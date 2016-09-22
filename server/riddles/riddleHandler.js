8fef48a1e87e6a3fca793d5db715e2ae05642f7c
Kyle Van Vleck <kylevanvleck@gmail.com>
2016-09-21 21:42:57 -0500

    set up stub for riddle handler methods on server

---
+++ server/riddles/riddleHandler.js
@@ -0,0 +1,11 @@
+// require database riddle model js file
+// require database user model js file
+
+// export entire object of methods to routes.js
+module.exports = {
+
+  // TODO:
+  // Set up server api methods for riddles here
+  // Coordinate w/ frontend and db
+
+};
