ca5240428a7b03af267cfe31ef5c3c59d5d2a493
Kyle Van Vleck <kylevanvleck@gmail.com>
2016-09-23 14:59:15 -0500

    stubbed out controllers for adventure handling

--- server/handlers/adventureHandler.js
+++ server/handlers/adventureHandler.js
@@ -1,5 +1,5 @@
-// require database riddle model js file
-// require database user model js file
+var Adventure = require('../models/Adventures.js');
+var User = require('../models/Users.js');
 
 // export entire object of methods to routes.js
 module.exports = {
@@ -7,5 +7,25 @@ module.exports = {
   // TODO:
   // Set up server api methods for riddles here
   // Coordinate w/ frontend and db
+
+  addAdventure: function(req, res){},
+
+  forgetAdventure: function(req, res){},
+
+  createAdventure:  function(req, res){},
+
+  deleteAdventure:  function(req, res){},
+
+  fetchAllAdventures: function(req, res){},
+
+  fetchMyInProgressAdventures: function(req, res){},
+
+  fetchMyCreatedAdventures: function(req, res){},
+
+  fetchSingleRiddle: function(req, res){}
+
+
+
+
 
 };
