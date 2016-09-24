9dff43b6a61c92ae22798bb12f5b83b3ede15b7b
Michael Daof <michael.ivanhoe@gmail.com>
2016-09-24 14:34:22 -0700

    (test) Mocha is setup - ready to integrate assertion library

--- spec/test.js
+++ spec/test.js
@@ -2,6 +2,12 @@ var assert = require('assert');
 
 describe('Mocha', function() {
   describe('#Mocha', function() {
-    it('should exist');
+
+    // Having no callback to do assertion tests will label the test as "pending"
+    // This system is good for writing speculative tests to be completed later.
+    it('should exist'/*, function (){...}*/);
+    it('should do something else', function (){
+      assert.equal("This test runs", "and fails equality assertion")
+    })
   });
 });
