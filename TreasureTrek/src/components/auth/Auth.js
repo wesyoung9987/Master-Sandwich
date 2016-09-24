ca80704fb1bc89ec3c9085a58dcabbd83e614cbf
alibhatti99 <alibhatti99@gmail.com>
2016-09-24 13:40:29 -0700

    Auth

--- TreasureTrek/src/components/auth/Auth.js
+++ TreasureTrek/src/components/auth/Auth.js
@@ -6,7 +6,7 @@ import t from 'tcomb-form-native';
 // App components
 import Main from '../main/Main.js'
 
-// Create Session Storage Key
+// Create Session Storage Key for AsyncStorage
 var STORAGE_KEY = 'id_token';
 
 var Form = t.form.Form;
