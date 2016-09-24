8283a28a9d85cc7bd5186324621b55d99dfcd7c8
Michael Daof <michael.ivanhoe@gmail.com>
2016-09-24 14:47:19 -0700

    (test) prepping TravisCI integration

--- spec/test.js
+++ spec/test.js
@@ -3,11 +3,18 @@ var assert = require('assert');
 describe('Mocha', function() {
   describe('#Mocha', function() {
 
-    // Having no callback to do assertion tests will label the test as "pending"
+    // Having no callback for assertion tests will label the test as "pending",
+    // just as it would with tests with an "x" by it.
     // This system is good for writing speculative tests to be completed later.
-    it('should exist'/*, function (){...}*/);
-    it('should do something else', function (){
-      assert.equal("This test runs", "and fails equality assertion")
-    })
+    it('Someone should fill this test out'/*, function (){...}*/);
+
+    xit('Fails if assertion is not equal', function (){
+      assert.equal("and fail", "an assertion test")
+    });
   });
+  describe('#Some Unit', function (){
+    it('A new section for a new unit to test', function (){
+      assert.equal("value", "value")
+    })
+  })
 });
