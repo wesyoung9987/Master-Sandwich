f0c24d9d88a2a91103de031342c1ef47b907d946
Kyle Van Vleck <kylevanvleck@gmail.com>
2016-09-27 17:27:39 -0500

    Added fetch api call to myAdventures root component

--- TreasureTrek/src/components/MyAdventures/myAdventuresContainer.js
+++ TreasureTrek/src/components/MyAdventures/myAdventuresContainer.js
@@ -1,14 +1,67 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, AsyncStorage, AlertIOS } from 'react-native';
 import Header from './../auth/Header.js';
 import MyAdventuresList from './myAdventuresList.js';
+import Auth from '../auth/Auth.js';
 
-const myAdventuresContainer = (props) => {
-  return (
-    <View style={styles.containerStyle}>
-      <MyAdventuresList nav={props}></MyAdventuresList>
-    </View>
-  );
+class myAdventuresContainer extends Component {
+
+  state = {
+    adventures: []
+  };
+
+  getMyAdventures () {
+    AsyncStorage.getItem('id_token')
+      .then(token=>{
+        fetch("https://treasure-trek.herokuapp.com/api/fetchMine",{
+            method: "GET",
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'x-access-token': token
+            }
+        }).then(function (res){
+          return res.json()
+        }).then((data)=> {
+          // set state or do something else with data
+          this.setState({adventures: data});
+        }).catch((error) => {
+          console.log("ERROR:",error);
+          this.handleError();
+        }).done();
+      });
+  }
+
+  handleError () {
+    AsyncStorage.removeItem('id_token')
+      .then(()=>{
+        this.errorRedirectToLogin("No Session - Redirecting");
+      }).catch(error => {
+        console.log('AsyncStorage error: ' + error.message);
+        this.errorRedirectToLogin("Internal Error - Redirecting")
+      });
+  }
+
+  errorRedirectToLogin (message) {
+    AlertIOS.alert(message);
+    this.props.resetToRoute({
+      name: "Login",
+      component: Auth
+    });
+  }
+
+  componentWillMount () {
+    this.getMyAdventures();
+  }
+
+  render () {
+    // console.log("STATE:",this.state);
+    return (
+      <View style={styles.containerStyle}>
+        <MyAdventuresList nav={this.props}></MyAdventuresList>
+      </View>
+    );
+  }
 };
 
 const styles = {
