b8275e3f0de05179fd1ee2b89551d3ec2fd69ecb
Michael Daof <michael.ivanhoe@gmail.com>
2016-09-27 17:13:47 -0700

    (feat) implemented RESTful API

--- TreasureTrek/src/components/allAdvens/AllAdventuresContainer.js
+++ TreasureTrek/src/components/allAdvens/AllAdventuresContainer.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, AsyncStorage } from 'react-native';
 import AllAdventuresList from './AllAdventuresList.js';
 
-const AllAdventures = (props) => {
-  return (
-    <View>
-      <AllAdventuresList nav={props}></AllAdventuresList>
-    </View>
-  );
+class AllAdventures extends Component {
+  constructor(){
+    super()
+    this.state = {
+      fetching: true,
+      recieved: false,
+      adventures: []
+    }
+  }
+
+  getAllAds(){
+    var self = this
+    AsyncStorage.getItem('id_token')
+    .then(function(data){
+      fetch("https://treasure-trek.herokuapp.com/api/fetchAll", {
+        headers: {
+          "x-access-token": data
+        }
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log("AllAdventures: ", data)
+        self.setState({
+          adventures: data
+        })
+      })
+    })
+  }
+
+  componentWillMount(){
+    this.getAllAds()
+  }
+
+  render(){
+    return (
+      <View>
+      <AllAdventuresList nav={this.props} advens={this.state.adventures}></AllAdventuresList>
+      </View>
+    );
+  }
 };
 
 export default AllAdventures
