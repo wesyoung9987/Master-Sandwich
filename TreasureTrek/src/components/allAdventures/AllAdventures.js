0ab39593371cc47b0cb274e54e2f85629abb06df
Michael Daof <michael.ivanhoe@gmail.com>
2016-09-28 09:41:20 -0700

    test

---
+++ TreasureTrek/src/components/allAdventures/AllAdventures.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+// app components
+
+class AllAdventures extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{
+          fontSize: 8
+        }}>Welcome to All Adventures</Text>
+      </View>
+
+    );
+  }
+}
+
+export default AllAdventures
