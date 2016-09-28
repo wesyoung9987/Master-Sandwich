243a78ad13f3e0d82ef25f84ce602b5d838ead08
Kyle Van Vleck <kylevanvleck@gmail.com>
2016-09-28 17:39:19 -0500

    added lat and long to adventures db model

--- server/models/Adventures.js
+++ server/models/Adventures.js
@@ -3,7 +3,14 @@ var mongoose = require('mongoose');
 var AdventureSchema = new mongoose.Schema({
   title: String,
   creator: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-  adventure: [{riddle: String, answer: String, location: String, photo: String }],
+  adventure: [{
+    riddle: String,
+    answer: String,
+    location: String,
+    longitude: String, // Maybe number, depends on google api
+    latitude: String, // Maybe number, depends on google api
+    photo: String
+  }],
   date: { type: Date, default: Date.now() },
   completedAll: { type: Boolean, default: false },
   startingLocation: String
