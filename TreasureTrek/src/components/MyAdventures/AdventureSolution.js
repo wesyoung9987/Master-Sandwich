58733b008880eeb13ff7cc9781f762def69fba9b
alibhatti99 <alibhatti99@gmail.com>
2016-09-30 20:07:12 -0700

    (feat) Delete and routing added to MyAdventures

--- TreasureTrek/src/components/MyAdventures/AdventureSolution.js
+++ TreasureTrek/src/components/MyAdventures/AdventureSolution.js
@@ -1,9 +1,58 @@
 import React from 'react';
-import {Text, View, TouchableHighlight, ScrollView} from 'react-native';
+import {AsyncStorage, AlertIOS, Text, View, TouchableHighlight, ScrollView} from 'react-native';
 import Riddle from './Riddle.js'
-//import Toggle from 'react-native-toggle';
+import MyAdventures from './myAdventuresContainer'
+import MenuButton from '../nav/MenuButton';
+
 
 const AdventureSolution = (props) => {
+
+  const deleteAdventure = function() {
+  AsyncStorage.getItem('id_token')
+    .then(token=>{
+      fetch("https://treasure-trek.herokuapp.com/api/forgetAd",{
+        method: "DELETE",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'x-access-token': token
+        },
+        body: JSON.stringify({adventureid: props.myAdventure.adventureId._id})
+      }).then(function(res){
+        return res.json()
+      }).then((data)=>{
+        console.log('Deleted! Data Response: ', data);
+        AlertIOS.alert('Adventure Deleted');
+        props.nav.resetToRoute({
+            name: "My Adventures",
+            component: MyAdventures,
+            leftCorner: MenuButton,
+            name: "My Adventures"
+          });
+      }).catch((error) => {
+        console.log('Delete ERROR: ', error);
+        handleError();
+      }).done();
+    });
+}
+
+  const handleError = function() {
+    AsyncStorage.removeItem('id_token')
+      .then(()=>{
+        errorRedirectToLogin("No Session - Redirecting");
+      }).catch(error => {
+        console.log('AsyncStorage error: ' + error.message);
+        errorRedirectToLogin("Internal Error - Redirecting")
+      });
+  }
+
+ const errorRedirectToLogin = function(message) {
+    AlertIOS.alert(message);
+    props.nav.resetToRoute({
+      name: "Login",
+      component: Auth
+    });
+  }
 
   return (
       <View style={{ flex: 1, marginTop:75 }}>
@@ -15,11 +64,13 @@ const AdventureSolution = (props) => {
           <Riddle num={2} completion={props.myAdventure.completion[1]} id={props.myAdventure.adventureId._id} nav={props.nav} loc={props.myAdventure.adventureId.adventure[1].location} riddle={props.myAdventure.adventureId.adventure[1].riddle} answer={props.myAdventure.adventureId.adventure[1].answer}></Riddle>
           <Riddle num={3} completion={props.myAdventure.completion[2]} id={props.myAdventure.adventureId._id} nav={props.nav} loc={props.myAdventure.adventureId.adventure[2].location} riddle={props.myAdventure.adventureId.adventure[2].riddle} answer={props.myAdventure.adventureId.adventure[2].answer}></Riddle>
           <View style={styles.giveup}>
-            <TouchableHighlight style={styles.button}  underlayColor='#99d9f4'>
-                <Text style={styles.buttonText}> Give Up? </Text>
+            <TouchableHighlight style={styles.button} onPress={deleteAdventure}  underlayColor='#00ffff'>
+                <Text  style={styles.buttonText}> Give Up? </Text>
             </TouchableHighlight>
           </View>
+
         </ScrollView>
+
       </View>
     );
 };
