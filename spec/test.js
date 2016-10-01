7db4aa5eac4dd1d3a041ea23714490356d3902e2
Michael Daof <michael.ivanhoe@gmail.com>
2016-10-01 14:19:40 -0700

    (test) setup db test

--- spec/test.js
+++ spec/test.js
@@ -1,23 +1,24 @@
-var assert = require('assert');
+// var assert = require('assert');
 
-describe('Mocha', function() {
-  describe('#Mocha', function() {
-
-    // Having no callback for tests will label the test as "pending",
-    // just as it would with tests with an "x" by it.
-    // This system is good for writing speculative tests to be completed later.
-    it('Someone should fill this test out'/*, function (){...}*/);
-
-    xit('Fails if assertion is not equal', function (){
-      assert.equal("and fail", "an assertion test")
-    });
-  });
-  describe('#Some Unit', function (){
-    it('A new section for a new unit to test', function (){
-      assert.equal("value", "value")
-    })
-    xit("An added test", function (){
-      assert.equal("runs before merge?", "runs before merge?")
-    })
-  })
-});
+// Mocha check
+// xdescribe('Mocha', function() {
+//   describe('#Mocha', function() {
+//
+//     // Having no callback for tests will label the test as "pending",
+//     // just as it would with tests with an "x" by it.
+//     // This system is good for writing speculative tests to be completed later.
+//     it('Someone should fill this test out'/*, function (){...}*/);
+//
+//     xit('Fails if assertion is not equal', function (){
+//       assert.equal("and fail", "an assertion test")
+//     });
+//   });
+//   describe('#Some Unit', function (){
+//     it('A new section for a new unit to test', function (){
+//       assert.equal("value", "value")
+//     })
+//     xit("An added test", function (){
+//       assert.equal("runs before merge?", "runs before merge?")
+//     })
+//   })
+// });
