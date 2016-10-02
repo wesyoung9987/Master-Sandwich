5d663271a7f42d0a73d66e62ab1e62406b4ef075
Wes Young <wes.young14@gmail.com>
2016-10-01 22:39:55 -0600

    My Profile page up and pulling data from the database

--- TreasureTrek/src/components/UserProfile/ProfileMain.js
+++ TreasureTrek/src/components/UserProfile/ProfileMain.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, AsyncStorage, AlertIOS } from 'react-native';
+import { View, Text, AsyncStorage, AlertIOS, Image, TouchableHighlight } from 'react-native';
 import UserImage from './UserImage';
 
 class UserProfile extends Component {
@@ -56,11 +56,114 @@ class UserProfile extends Component {
   render(){
     return (
       <View style={{ flex: 1}}>
-        <UserImage/>
-        <Text>{this.state.myInfo.username}</Text>
+        <View style={styles.imageContainer}>
+
+          <UserImage/>
+
+        </View>
+        <View style={styles.fixToBottom}>
+          <View style={styles.infoContainer}>
+            <View style={styles.contentStyle}>
+              <Image style={styles.imageStyle} source={require('../../resources/spinning-coin.gif')} />
+            </View>
+            <View style={styles.contentStyle}>
+
+              <Text style={styles.textInfo}>15000</Text>
+
+            </View>
+
+          </View>
+          <View style={styles.infoContainer}>
+            <View style={styles.contentStyle}>
+              <Text style={styles.textInfo}>Level 5</Text>
+            </View>
+          </View>
+          <View style={styles.infoContainer}>
+            <View style={styles.contentStyle}>
+              <Text style={styles.textInfo}>500 Coins to Next Level</Text>
+            </View>
+          </View>
+          <View style={styles.infoContainer}>
+            <View style={styles.contentStyle}>
+              <Text style={styles.textInfo}>Adventures in Progress: 3</Text>
+            </View>
+          </View>
+          <View style={styles.infoContainer}>
+            <View style={styles.contentStyle}>
+              <Text style={styles.textInfo}>Created Adventures: 7</Text>
+            </View>
+          </View>
+          <View style={styles.infoContainer2}>
+            <View style={styles.contentStyle}>
+              <Text style={styles.textInfo2}>Email: {this.state.myInfo.email}</Text>
+            </View>
+          </View>
+          <TouchableHighlight
+
+            underlayColor={'#00ffff'}>
+            <View style={styles.infoContainer2}>
+              <View style={styles.contentStyle}>
+                <Text style={styles.textInfo2}>Update Profile Picture</Text>
+              </View>
+            </View>
+          </TouchableHighlight>
+        </View>
+
       </View>
     );
   }
 };
+
+const styles = {
+  imageContainer: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 10
+  },
+  contentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  imageStyle: {
+    height: 25,
+    width: 25,
+    marginRight: 10
+  },
+  textInfo: {
+    color: 'white',
+    fontSize: 20,
+  },
+  infoContainer: {
+    height: 45,
+    backgroundColor: '#48BBEC',
+    borderColor: 'white',
+    borderWidth: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    zIndex: 1,
+    flexDirection: 'row',
+  },
+  infoContainer2: {
+    height: 45,
+    backgroundColor: 'white',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    zIndex: 1,
+    flexDirection: 'row',
+  },
+  textInfo2: {
+    color: '#48BBEC',
+    fontSize: 20,
+    marginLeft: 5
+  },
+  fixToBottom: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0
+  }
+}
 
 export default UserProfile;
