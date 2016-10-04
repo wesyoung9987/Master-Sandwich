08ef08d756a21df0f95458ada7cc65a3cdc800b7
Wes Young <wes.young14@gmail.com>
2016-10-04 15:10:28 -0600

    Add profile photo page

---
+++ TreasureTrek/src/components/UserProfile/UpdatePicButton.js
@@ -0,0 +1,55 @@
+import React, { Component, StyleSheet, DeviceEventEmitter } from 'react';
+import { View, Text, TouchableHighlight } from 'react-native';
+import UploadPhoto from './UploadPhoto';
+
+const UpdatePicButton = (props) => {
+
+  const uploadPicRoute = {
+    name: 'Upload Profile Picture',
+    component: UploadPhoto,
+    passProps: {
+      adven: props.nav
+    }
+  }
+
+  var toUploadPic = function (){
+    props.nav.toRoute(uploadPicRoute);
+  }
+
+  return (
+    <TouchableHighlight
+      onPress={toUploadPic}
+      underlayColor={'#00ffff'}>
+      <View style={styles.infoContainer2}>
+        <View style={styles.contentStyle}>
+          <Text style={styles.textInfo2}>Update Profile Picture</Text>
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
+
+};
+
+const styles = {
+  infoContainer2: {
+    height: 45,
+    backgroundColor: 'white',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    zIndex: 1,
+    flexDirection: 'row',
+  },
+  textInfo2: {
+    color: '#48BBEC',
+    fontSize: 20,
+    marginLeft: 5
+  },
+  contentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  }
+}
+
+export default UpdatePicButton;
