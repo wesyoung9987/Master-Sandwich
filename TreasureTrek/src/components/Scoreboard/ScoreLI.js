8825f31fbc9d207ef5057c18134e0a7abd7e1c06
Kyle Van Vleck <kylevanvleck@gmail.com>
2016-10-08 17:13:23 -0500

    fixed scoreboard list items for all phone models

--- TreasureTrek/src/components/Scoreboard/ScoreLI.js
+++ TreasureTrek/src/components/Scoreboard/ScoreLI.js
@@ -9,22 +9,29 @@ import {
 
 const ScoreLI = (props) => {
 
-  const { viewStyle, detailsStyle, arrowsStyle, titleStyle, arrowsContainer } = styles;
+  const { viewStyle, detailsStyleL, detailsStyleR, arrowsStyle, titleStyle, pointStyle, arrowsContainer } = styles;
 
   return (
     <TouchableHighlight
-    underlayColor={'#00ffff'}>
+      underlayColor={'#00ffff'}>
       <View style={viewStyle}>
-         <View style={arrowsContainer}>
-          <Image style={arrowsStyle} source={require('../../resources/user-placeholder.png')} />
+
+        <View style={{flexDirection: 'row', justifyContent: 'flex-start', flex: 1}}>
+          <View style={arrowsContainer}>
+            <Image style={arrowsStyle} source={require('../../resources/user-placeholder.png')} />
+          </View>
+
+
+          <View style={detailsStyleL}>
+            <Text style={titleStyle}>{props.score.username}</Text>
+            <Text style={{ fontSize: 15 }}>Level {props.score.level}</Text>
+          </View>
         </View>
-        <View style={detailsStyle}>
-          <Text style={titleStyle}>{props.score.username}</Text>
-          <Text style={{ fontSize: 15 }}>Level {props.score.level}</Text>
+
+        <View style={detailsStyleR}>
+          <Text style={pointStyle}>{props.score.points}</Text>
         </View>
-        <View style={detailsStyle}>
-          <Text style={titleStyle}>{props.score.points}</Text>
-        </View>
+
       </View>
     </TouchableHighlight>
     );
@@ -36,17 +43,33 @@ const styles = {
     flexDirection: 'row',
     borderWidth: 1,
     borderColor: '#ddd',
-    padding: 10
+    padding: 10,
+    justifyContent: 'space-between'
   },
-  detailsStyle: {
+  detailsStyleL: {
     flexDirection: 'column',
+    flex: 1
     //justifyContent: 'space-around',
-    marginLeft: 5,
-    width: 245
+    // marginLeft: 5,
+    // width: 245
+  },
+  detailsStyleR: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    marginLeft: 5
+    //justifyContent: 'space-around',
+    // marginLeft: 5,
+    // width: 245
   },
   titleStyle: {
     fontSize: 24,
-    color: '#ed1558'
+    color: '#ed1558',
+    alignItems: 'center',
+    flex: 1
+  },
+  pointStyle: {
+    fontSize: 24,
+    color: '#ed1558',
   },
   arrowsStyle: {
     height: 35,
@@ -56,6 +79,7 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+    marginRight: 5
   }
 };
 
